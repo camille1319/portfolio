b2ef0bc83d38937893430ce609226c1eb3d37802
camille1319 <camillevictoria.contato@gmail.com>
2024-10-31 21:14:42 -0300

    alteracoes de portfolio camille

--- src/componentes/rodape.js
+++ src/componentes/rodape.js
@@ -2,7 +2,7 @@ export function Rodape(props){
   return (
     <div>
         <p>&copy; 2024 TechInclusão. Todos os direitos reservados.</p>
-      <div>
+      <div className="links">
         <a href={props.linkGithub} target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href={props.linkLinkedin}>LinkedIn</a>
         <a href={props.linkEmail}>Email</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import { Rodape } from '../componentes/rodape';
 import Favoritos from '../componentes/favoritos'; // Importando o novo componente
 
 const projetos = [
-  { nome: 'Projeto A', descricao: 'Descrição do projeto A' },
-  { nome: 'Projeto B', descricao: 'Descrição do projeto B' },
-  { nome: 'Projeto C', descricao: 'Descrição do projeto C' },
+  { nome: 'sobre mim', descricao: 'Sou a Camille Victória Rodrigues Silva, tenho 17 anos. Atualmente moro em Palmas-TO, estou no segundo ano do ensino médio e trabalho como assistente admnistrativo de menor aprendiz pela instituição Renapsi.' },
+  { nome: 'Experiencias de trabalho', descricao: 'Profissão que tenho experiencias: Assistente administrativo , auxiliar de atendente, cuidadora de crianças.' },
+  { nome: 'Objetivos', descricao: 'Este projeto tem como obetivo mostrar aos leitores a minha capacidade de programadora , mostrar as minhas experiencias de trabalho, e um pouco de quem eu sou.' },
 ];
 
 export default function Home() {
@@ -19,10 +19,10 @@ export default function Home() {
       <Cabecalho
         nome="Meu Portfólio"
         subtitulo="Bem-vindo ao meu espaço"
-        descricao="Aqui você encontrará uma seleção dos meus projetos e experiências."
+        descricao="Aqui você encontrará algumas informações sobre mim ."
       />
       <Likes/>
-      <Titulo nome="Projetos" subnome="Alguns dos meus trabalhos recentes" />
+      <Titulo nome="informações" subnome="Algumas informações recentes" />
      {/*  <section className="projetos">
         {projetos.map((projeto, index) => (
           <Cartao
@@ -39,7 +39,7 @@ export default function Home() {
       <Rodape 
         linkGithub="https://github.com/JoaoIto"
         linkLinkedin="https://www.linkedin.com/in/joao-ito-b27b821b9/"
-        linkEmail="mailto:joaoIto@gmail.com"
+        linkEmail="camillevictoria.contato@gmail.com"
       />
     </div>
   );
